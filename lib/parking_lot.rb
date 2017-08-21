c256deb1e5b81dca7d01237a6d1253e5068de390
Sahil Batla <sahil.batla@wingify.com>
2017-08-21 11:02:14 +0530

    Use two space indentation

--- lib/parking_lot.rb
+++ lib/parking_lot.rb
@@ -1,69 +1,69 @@
 require_relative './slots'
 
 class ParkingLot
-	attr_accessor :slots
+  attr_accessor :slots
 
-	def initialize(number_of_slots)
-		@slots = []
-		number_of_slots.to_i.times do |index|
-			slot_number = index + 1
-			slots[index] = Slot.new(slot_number)
-		end
-		puts "Create a parking lot with #{ number_of_slots } slots"
-	end
+  def initialize(number_of_slots)
+    @slots = []
+    number_of_slots.to_i.times do |index|
+      slot_number = index + 1
+      slots[index] = Slot.new(slot_number)
+    end
+    puts "Create a parking lot with #{ number_of_slots } slots"
+  end
 
-	def park(vehicle_name, vehicle_number)
-		if next_free_slot
-			puts "Allocated slot number : #{ next_free_slot.id }"
-			next_free_slot.park(vehicle_name, vehicle_number)
-		else
-			parking_lot_full_handler
-		end
-	end
+  def park(vehicle_name, vehicle_number)
+    if next_free_slot
+      puts "Allocated slot number : #{ next_free_slot.id }"
+      next_free_slot.park(vehicle_name, vehicle_number)
+    else
+      parking_lot_full_handler
+    end
+  end
 
-	def leave(slot_number)
-		slots[slot_number.to_i - 1].free
-		puts "Slot number #{ slot_number.to_i - 1 } is free "
-	end
+  def leave(slot_number)
+    slots[slot_number.to_i - 1].free
+    puts "Slot number #{ slot_number.to_i - 1 } is free "
+  end
 
-	def status
-		puts "Slot No.\t Registration Number\t Colour"
-		slots.each do | slot |
-			puts "#{ slot.id }\t\t #{ slot.vehicle_number }\t\t #{ slot.vehicle_color }" unless (slot.free?)
-		end
-	end
+  def status
+    puts "Slot No.\t Registration Number\t Colour"
+    slots.each do | slot |
+      puts "#{ slot.id }\t\t #{ slot.vehicle_number }\t\t #{ slot.vehicle_color }" unless (slot.free?)
+    end
+  end
 
-	def registration_numbers_for_cars_with_colour (color)
-		filtered_cars = filter_cars('vehicle_number', 'vehicle_color', color)
-		puts filtered_cars.compact.join(',')
-	end
+  def registration_numbers_for_cars_with_colour (color)
+    filtered_cars = filter_cars('vehicle_number', 'vehicle_color', color)
+    puts filtered_cars.compact.join(',')
+  end
 
-	def slot_numbers_for_cars_with_colour (color)
-		filtered_cars = filter_cars('id', 'vehicle_color', color)
-		puts filtered_cars.compact.join(',')
-	end
+  def slot_numbers_for_cars_with_colour (color)
+    filtered_cars = filter_cars('id', 'vehicle_color', color)
+    puts filtered_cars.compact.join(',')
+  end
 
-	def slot_number_for_registration_number (vehicle_number)
-		slot = slots.find do |slot|
-			slot.vehicle_number == vehicle_number
-		end
-		puts slot ? slot.id : 'Not Found'
-	end
+  def slot_number_for_registration_number (vehicle_number)
+    slot = slots.find do |slot|
+      slot.vehicle_number == vehicle_number
+    end
+    puts slot ? slot.id : 'Not Found'
+  end
 
-	private
-		def next_free_slot
-			slots.find do |slot|
-				slot.free?
-			end
-		end
+  private
+    def next_free_slot
+      slots.find do |slot|
+        slot.free?
+      end
+    end
 
-		def parking_lot_full_handler
-			puts "Sorry, parking lot is full"
-		end
+    def parking_lot_full_handler
+      puts "Sorry, parking lot is full"
+    end
 
-		def filter_cars(filtered_value, filter_by, filter)
-			slots.collect do |slot|
-				slot.send(filtered_value) if slot.send(filter_by) == filter
-			end
-		end
+    def filter_cars(filtered_value, filter_by, filter)
+      slots.collect do |slot|
+        slot.send(filtered_value) if slot.send(filter_by) == filter
+      end
+    end
 end
