81abeb9d7852bb1a01546d0a9e3257c84090eb23
Sahil Batla <sahil.batla@wingify.com>
2017-08-26 00:12:18 +0530

    Convert slot_number to integer in leave

--- lib/parking_lot.rb
+++ lib/parking_lot.rb
@@ -25,8 +25,9 @@ class ParkingLot
   end
 
   def leave(slot_number)
+    slot_number = slot_number.to_i
     if slot_number > 0 && slot_number <= slots.length
-      slots[slot_number.to_i - 1].free
+      slots[slot_number - 1].free
       puts "Slot number #{ slot_number.to_i - 1 } is free "
     else
       puts "Invalid slot number"
